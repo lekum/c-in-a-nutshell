03e9d8f4d755a37ebdbd5b94c207c71e8e62a68a
Alejandro Guirao <lekumberri@gmail.com>
2018-11-02 12:20:28 +0100

    Add example of float and double precision

---
+++ chapter_2/float_precision/float_precision.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <float.h>
+
+int main()
+{
+  puts("\nCharacteristics of the type float\n");
+  printf("Storage size: %zu bytes\n"
+      "Smallest positive value: %E\n"
+      "Greatest positive value: %E\n"
+      "Precision: %d decimal digits\n",
+      sizeof(float), FLT_MIN, FLT_MAX, FLT_DIG);
+  puts("\nAn example of float precision:\n");
+  double d_var = 12345.6;
+  // A variable of type double.
+  float f_var = (float)d_var; // Initializes the float
+  // variable with the value of d_var.
+  printf("The floating-point number"
+      "%18.10f\n", d_var);
+  printf("has been stored in a variable\n"
+      "of type float as the value "
+      "%18.10f\n", f_var);
+  printf("The rounding error is "
+      "%18.10f\n", d_var - f_var);
+  return 0;
+}
