9bff9f7f23eb56b5ce2ab05f0230ef842e2a0438
Alejandro Guirao <lekumberri@gmail.com>
2018-12-04 08:11:25 +0100

    Add more example of glibc open

--- chapter_4/glibc/glibc.c
+++ chapter_4/glibc/glibc.c
@@ -7,9 +7,27 @@
 int main()
 {
   printf("The GLIBC version present is %s\n", gnu_get_libc_version());
-  int fd = open("kk.txt", O_WRONLY | O_CREAT | O_TRUNC, 0400);
+
+  int fd = open("startup", O_RDONLY);
   if (fd == -1) {
     perror("open");
     exit(EXIT_FAILURE);
   }
+
+  /* Open new or existing file for reading and writing, truncating tyo zero
+   * bytes; file permissiones read+write for owner, nothing for all others */
+  fd = open("myfile", O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+  if (fd == -1) {
+    perror("open");
+    exit(EXIT_FAILURE);
+  }
+
+  /* Open new or existing file for writing; writes should always append to end
+   * of file */
+  fd = open("w.log", O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, S_IRUSR | S_IWUSR);
+  if (fd == -1) {
+    perror("open");
+    exit(EXIT_FAILURE);
+  }
+
 }
